7a62afecfbf47212aa6db235218e80c9829df555
Tinh Pham <phamvantinhk37@gmail.com>
2026-01-04 16:29:04 +0700

    Add env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,11 @@
  * Tệp cấu hình môi trường cho ứng dụng.
  * Trong thực tế, bạn sẽ có environment.ts cho phát triển và environment.prod.ts cho triển khai.
  */
-import {env} from "@ngx-env/builder/dist/builders/c";
-
+import {env} from "@ngx-env/builder";
 console.log(env);
 export const environment = {
   production: false,
-  youtubeApiUrl: 'http://localhost:5000',
+  youtubeApiUrl: 'https://youtube-uploader-be.onrender.com/',
   geminiApiKey: "AIzaSyBvZLI52YsfO3XqiwJ5euYLjpsYYjvUpLA", // Khóa API để trống để đảm bảo bảo mật khi chia sẻ code
-  appId: "youtube-uploader-dev"
+  appId: "youtube-uploader-prod"
 };
